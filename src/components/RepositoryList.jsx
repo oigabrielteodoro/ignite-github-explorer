ff63fb3c317fc0405d778b3e0171bb71675923fe
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-16 22:29:25 -0300

    feat: added axios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import axios from 'axios';
+
 import RepositoryItem from "./RepositoryItem";
 
 import '../styles/repositories.scss';
@@ -9,15 +13,22 @@ const repository = {
 }
 
 export default function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/orgs/rocketseat/repos').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => (
+          <RepositoryItem repository={repository} />
+        ))}
       </ul>
     </section>
   )
