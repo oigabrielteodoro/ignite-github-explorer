b94c3ed46ced6fda50795cfd52f1303b5a9bc9ce
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-16 22:26:27 -0300

    feat: added repositories list styles

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  },
+  }, 
   resolve: {
     extensions: ['.js', '.jsx']
   },
@@ -27,7 +27,7 @@ module.exports = {
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.scss$/,
