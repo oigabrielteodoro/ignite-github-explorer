b74962198ec0eac21589a827e571494f36625a45
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-16 22:49:59 -0300

    feat: migrate all files to tsx

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -6,8 +6,15 @@ import RepositoryItem from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+  id: string;
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export default function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     axios.get('https://api.github.com/orgs/rocketseat/repos').then(response => {
